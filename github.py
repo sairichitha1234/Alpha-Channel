4def92d6257df77a960190456d2b786bed4bde33
sairichitha1234 <sairichitha1234@gmail.com>
2020-09-08 20:18:01 +0530

    My first File

---
+++ github.py
@@ -0,0 +1,13 @@
+import cv2 
+import numpy as np
+img=cv2.imread("sun.jpg")
+cv2.imwrite("sun.png",img)
+src = cv2.imread("sun.png", 1)
+tmp = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+_,alpha = cv2.threshold(tmp,0,255,cv2.THRESH_BINARY)
+b, g, r = cv2.split(src)
+rgba = [b,g,r, alpha]
+dst = cv2.merge(rgba,4)
+cv2.imwrite("test.png", dst)
+
+
