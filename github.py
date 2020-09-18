6abffd28d6af83b136b7648144710c4f104aeada
sairichitha1234 <67508396+sairichitha1234@users.noreply.github.com>
2020-09-18 11:59:36 +0530

    Update github.py

--- github.py
+++ github.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 img=cv2.imread("sun.jpg")
 cv2.imwrite("sun.png",img)
-src = cv2.imread("sun.png", 0)
+src = cv2.imread("sun.png")
 tmp = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
 _,alpha = cv2.threshold(tmp,1,255,cv2.THRESH_BINARY)
 b, g, r = cv2.split(src)
